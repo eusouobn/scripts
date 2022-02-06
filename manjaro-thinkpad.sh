0924a7036a3bc2c5894bd582b45bcd9ab780487e
eusouobn <86791806+eusouobn@users.noreply.github.com>
2022-02-06 15:01:15 -0300

    Update manjaro-thinkpad.sh

--- manjaro-thinkpad.sh
+++ manjaro-thinkpad.sh
@@ -4,8 +4,9 @@ echo -e "\n#\n#\n#\n#\n#\n#\n#\n#\n#\n###GRUB###\n#\n#\n#\n#\n#\n#\n#\n#\n#\n"
 
 sleep 1
 
-sudo sed -i '2c\GRUB_TIMEOUT=1' /etc/default/grub && sudo sed -i '5c\GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 mitigations=off psmouse.synaptics_intertouch=0"' /etc/default/grub && sudo grub-mkconfig -o /boot/grub/grub.cfg
+sudo sed -i '2c\GRUB_TIMEOUT=1' /etc/default/grub && sudo sed -i '5c\GRUB_CMDLINE_LINUX_DEFAULT="ipv6.disable=1 mitigations=off amdgpu.ppfeaturemask=0xffffffff"' /etc/default/grub && sudo grub-mkconfig -o /boot/grub/grub.cfg
 
+#
 #
 #
 #
@@ -34,6 +35,17 @@ sudo pacman -S simplescreenrecorder ffmpeg psensor gimp gnome-disk-utility frei0
 #
 #
 #
+
+echo -e "\n#\n#\n#\n#\n#\n#\n#\n#\n#\n###WINE###\n#\n#\n#\n#\n#\n#\n#\n#\n#\n"
+
+sudo pacman -S wine-staging wine-mono wine-gecko giflib lib32-giflib libpng lib32-libpng libldap lib32-libldap gnutls lib32-gnutls mpg123 lib32-mpg123 openal lib32-openal v4l-utils lib32-v4l-utils libpulse lib32-libpulse libgpg-error lib32-libgpg-error alsa-plugins lib32-alsa-plugins alsa-lib lib32-alsa-lib libjpeg-turbo lib32-libjpeg-turbo sqlite lib32-sqlite libxcomposite lib32-libxcomposite libxinerama lib32-libgcrypt libgcrypt lib32-libxinerama ncurses lib32-ncurses opencl-icd-loader lib32-opencl-icd-loader libxslt lib32-libxslt libva lib32-libva gtk3 lib32-gtk3 gst-plugins-base-libs lib32-gst-plugins-base-libs vulkan-icd-loader lib32-vulkan-icd-loader vkd3d lib32-vkd3d lib32-gnutls winetricks --noconfirm
+
+#
+#
+#
+#
+#
+
 
 echo -e "\n#\n#\n#\n#\n#\n#\n#\n#\n#\n###Chrome###\n#\n#\n#\n#\n#\n#\n#\n#\n#\n"
 
