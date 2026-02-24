3c7f0f8db00166da66c0e98c75a79739970aaf73
eusouobn <86791806+eusouobn@users.noreply.github.com>
2026-02-24 02:06:34 -0300

    Add files via upload

---
+++ ffmpeg-ezcap-split-audio-and-video.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+DESTINO="/mnt/videos"
+mkdir -p "$DESTINO"
+
+BASE="video_$(date +%Y-%m-%d_%H-%M-%S)"
+
+VIDEO="$DESTINO/${BASE}_video.mkv"
+AUDIO="$DESTINO/${BASE}_audio.wav"
+FINAL="$DESTINO/${BASE}_final.mkv"
+
+AUDIO_DEVICE="alsa_input.usb-ezcap_ezcap_GameLink_Neo_Pro_38200002-02.analog-stereo"
+
+echo ">>> Iniciando captura..."
+
+# Beep de sincronização
+speaker-test -t sine -f 1000 -l 1 >/dev/null 2>&1 &
+sleep 0.2
+
+########################################
+# CAPTURA DE VÍDEO
+########################################
+
+LIBVA_DRIVER_NAME=radeonsi ffmpeg \
+-init_hw_device vaapi=va:/dev/dri/renderD128 -filter_hw_device va \
+-extra_hw_frames 128 \
+-fflags nobuffer+genpts -flags low_delay \
+-use_wallclock_as_timestamps 1 \
+-thread_queue_size 8192 \
+-f v4l2 -input_format nv12 -video_size 2560x1440 -framerate 60 -i /dev/video0 \
+-vf "format=nv12,hwupload" \
+-c:v hevc_vaapi -rc_mode CQP -qp 26 \
+-an \
+"$VIDEO" &
+
+PID_VIDEO=$!
+
+########################################
+# CAPTURA DE ÁUDIO
+########################################
+
+ffmpeg \
+-use_wallclock_as_timestamps 1 \
+-fflags +genpts \
+-thread_queue_size 8192 \
+-f pulse -i "$AUDIO_DEVICE" \
+-c:a pcm_s16le \
+-af "aresample=async=1:first_pts=0,apad" \
+"$AUDIO" &
+
+PID_AUDIO=$!
+
+########################################
+
+echo ">>> Gravando... pressione ENTER para parar"
+read
+
+echo ">>> Parando captura..."
+
+kill -INT $PID_VIDEO
+kill -INT $PID_AUDIO
+
+wait
+
+echo ">>> Fazendo mux final..."
+
+ffmpeg \
+-fflags +genpts \
+-i "$VIDEO" \
+-i "$AUDIO" \
+-c copy \
+"$FINAL"
+
+echo ">>> Finalizado:"
+echo "$FINAL"
